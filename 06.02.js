d8361f9af243b2795f5fe34bdb694bf6ae77ec9e
Depyt <73955929+Depyt@users.noreply.github.com>
2021-02-06 18:55:32 +0200

    Add files via upload

---
+++ 06.02.js
@@ -0,0 +1,47 @@
+// let result = 0;
+// let arr = [1, 2, 3, 4, 5 ]
+// for(let i = 0; i < arr.length; i++ ){
+//     result += arr[i]
+// }
+// alert(result)
+
+// let obj = {
+//     Миша:'200',
+//     Влад:'300',
+//     Андрей:'404'
+// }
+// for(let key in obj){
+//     alert(key + ' - заплата '+ obj[key] + ' .' )
+// }
+
+// let arr = [2, 5, 9, 15, 0, 4 , 3];
+// for(let i = 0;i < arr.length; i++){
+//     if( arr[i] > 3 && arr[i] < 10){
+//         alert(arr[i])
+//     }   
+// }
+
+// let res = 0;
+// let arr = [-3, 3, -5, 7,-34 ,9];
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0 ){
+//         res += arr[i]
+//     }
+// }
+// alert(res)
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for(let i = 0; i <arr.length; i++){
+//     if(i === 4){
+//         alert('Yehu')
+//     }
+// }
+
+// let arr = [10, 20, 30, 50, 235, 3000];
+// for(let i = 0; i <arr.length; i++){
+//     if(arr[1] = 1,2,5){
+//         alert(arr)
+//     }
+// }
+
+
